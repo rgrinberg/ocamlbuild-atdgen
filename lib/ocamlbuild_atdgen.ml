202b5481b18d22c85e03cc36265eeefae0ebda54
Rudi Grinberg <rudi.grinberg@gmail.com>
2015-06-08 09:26:48 -0400

    small style improvements

--- lib/ocamlbuild_atdgen.ml
+++ lib/ocamlbuild_atdgen.ml
@@ -5,13 +5,12 @@ let () = Options.use_ocamlfind := true
 let atdgen = "atdgen"
 
 let tag_atdgen env patterns =
-  patterns
-  |> List.iter (fun p ->
+  patterns |> List.iter begin fun p ->
     ["package(atdgen)"]
     |> Tags.of_list
     |> Tags.elements
     |> tag_file (env p)
-  )
+  end
 
 let init () =
   rule "atdgen: .atd -> _t.ml*"
@@ -20,14 +19,14 @@ let init () =
     (begin fun env build ->
        tag_atdgen env ["%_t.ml";"%_t.mli"];
        Cmd (S [A atdgen; A "-t"; P (env "%.atd")]);
-     end) ;
+     end);
   rule "atdgen: .atd -> _j.ml*"
     ~prods:["%_j.ml";"%_j.mli";]
     ~dep:"%.atd"
     (begin fun env build ->
        tag_atdgen env ["%_j.ml"; "%_j.mli"];
        Cmd (S [A atdgen; A "-j"; A "-j-std"; P (env "%.atd")]);
-     end) ;
+     end);
   rule "atdgen: .atd -> _v.ml*"
     ~prods:["%_v.ml";"%_v.mli";]
     ~dep:"%.atd"
